ecd53caf5cfe06db338a7e98205fc809faa52f09
janbean <janbean@163.com>
2014-12-26 18:54:48 +0800

    add angular

---
+++ js/ngAirTickets.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("airTicketsApp", []);
+//通用函数
+app.factory('Util', ['$window',
+function(win) {
+	var Util = {};
+	
+	return Util;
+}]);
+//ajax函数
+app.factory('Ajax', ['$window',
+function(win) {
+	var Ajax = {};
+	
+	return Ajax;
+}]);
