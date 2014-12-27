bef035a8f67716760520949a69645d823f2d69f9
janbean <janbean@163.com>
2014-12-27 21:03:47 +0800

    add search function

--- js/index.js
+++ js/index.js
@@ -7,26 +7,6 @@ $(document).ready(function(){
 	$("#endPlace").select2({
 		data:[{id:0,text:'广州'},{id:2,text:'深圳'},{id:3,text:'上海'},{id:4,text:'南京'}]
 	});
-	$("#search").click(function(){  
-		if($("#startPlace").select2("data")==null){
-			alert("请选择出发地");
-			return ;
-		}
-		if($("#endPlace").select2("data")==null){
-			alert("请选择目的地");
-			return ;
-		}
-		//id
-		var startId = $("#startPlace").val();
-		//value
-		var startValue = $("#startPlace").select2("data").text;
-
-		//id
-		var endId = $("#endPlace").val();
-		//value
-		var endValue = $("#endPlace").select2("data").text;
-		alert("出发地:"+startValue+"  目的地:"+endValue);
-	});
 	
 	 $('#startDatepicker').datepicker();
 	 $('#endDatepicker').datepicker();
@@ -45,8 +25,6 @@ function($scope, Util, Database) {
 		$scope.activePage = [];
 		$scope.activePage[_page]=true;
 	}
-
-	activePageFun($scope.curPage);
 
 	function countTotalPages(){
 		var query = new Database.Query('Flight');
@@ -82,11 +60,31 @@ function($scope, Util, Database) {
 		});
 	};
 
+	$scope.search = function(){
+		if($("#startPlace").select2("data")==null){
+			alert("请选择出发地");
+			return ;
+		}
+		if($("#endPlace").select2("data")==null){
+			alert("请选择目的地");
+			return ;
+		}
+		//id
+		var startId = $("#startPlace").val();
+		//value
+		var startValue = $("#startPlace").select2("data").text;
+
+		//id
+		var endId = $("#endPlace").val();
+		//value
+		var endValue = $("#endPlace").select2("data").text;
+		alert("出发地:"+startValue+"  目的地:"+endValue);
+	}
+	
+	activePageFun($scope.curPage);
 	$scope.queryByPage($scope.curPage);
 	countTotalPages();
 	
-
-
 	// var flight = new Database.Object('Flight',{
 	// 	company:'天堂航空',
 	// 	planeName:'RP233 22(小)',
